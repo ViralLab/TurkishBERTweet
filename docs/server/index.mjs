fbfa4924b4c76fa66435c42e9baef12ec82dda65
AliNajafi <najafi1998ali@gmail.com>
2024-08-27 17:46:40 +0300

    webp updated init

---
+++ docs/server/index.mjs
@@ -0,0 +1,10 @@
+import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node:http';
+import 'node:https';
+export { n as default } from './chunks/runtime.mjs';
+import 'fs';
+import 'path';
+import 'node:fs';
+import 'node:url';
+import '@iconify/utils';
+import 'consola/core';
+//# sourceMappingURL=index.mjs.map
