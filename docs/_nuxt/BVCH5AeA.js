4665681be683ad1f92bfd40c0f109b8fea3273fa
AliNajafi <najafi1998ali@gmail.com>
2024-08-31 16:39:52 +0300

    try middleware

---
+++ docs/_nuxt/BVCH5AeA.js
@@ -0,0 +1,1 @@
+function a({route:e,redirect:r}){const t="/TurkishBERTweet/";if(!e.fullPath.toLowerCase().startsWith(t.toLowerCase())){const c=t+e.fullPath.slice(t.length);return r(c)}}export{a as default};
