b427eac4735f3c4c204a08aa3caad3a5a48bf694
AliNajafi <najafi1998ali@gmail.com>
2024-09-03 15:23:30 +0300

    finetune content added

---
+++ docs/_nuxt/VaQbr8R-.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,m as n}from"./Cl6uyuXL.js";const s={},_={class:"custom-html-content"};function r(t,a){return o(),c("div",_,[n(t.$slots,"default")])}const m=e(s,[["render",r]]);export{m as _};
